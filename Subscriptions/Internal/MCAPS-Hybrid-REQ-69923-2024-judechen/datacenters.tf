0d30742cd955c1ac09f77bd5af50fb4005bfbcc8
Jude Chen <judechen@microsoft.com>
2024-07-10 11:55:03 -0500

    test resource provider

--- Subscriptions/Internal/MCAPS-Hybrid-REQ-69923-2024-judechen/datacenters.tf
+++ Subscriptions/Internal/MCAPS-Hybrid-REQ-69923-2024-judechen/datacenters.tf
@@ -1,20 +1,6 @@
-variable "is_new_subscription" {
-  type    = bool
-  default = false
-}
-
 resource "azurerm_resource_provider_registration" "cognitiveservices" {
-  count = var.is_new_subscription ? 1 : 0
   name  = "Microsoft.CognitiveServices"
-  # lifecycle {
-  #   precondition {
-  #     condition     = var.is_new_subscription
-  #     error_message = "Resource provider is already registered!"
-  #   }
-  # }
-}
-
-resource "azurerm_resource_group" "datacenter_eastus" {
-  location = "eastus"
-  name     = "datacenter-eastus-rg"
+  lifecycle {
+    ignore_changes = all
+  }
 }

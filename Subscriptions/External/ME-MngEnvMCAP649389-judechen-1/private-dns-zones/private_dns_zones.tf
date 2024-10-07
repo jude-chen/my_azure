01722dd9fbed4936745f38ec6b9ca8c33483889d
Jude Chen <judechen@microsoft.com>
2024-10-07 16:42:00 -0500

    different rg name

--- Subscriptions/External/ME-MngEnvMCAP649389-judechen-1/private-dns-zones/private_dns_zones.tf
+++ Subscriptions/External/ME-MngEnvMCAP649389-judechen-1/private-dns-zones/private_dns_zones.tf
@@ -2,6 +2,6 @@ module "avm-ptn-network-private-link-private-dns-zones" {
   source  = "Azure/avm-ptn-network-private-link-private-dns-zones/azurerm"
   version = "0.4.0"
   # insert the 2 required variables here
-  resource_group_name = "private-dns-rg"
+  resource_group_name = "private-dns-zones-rg"
   location            = local.primary_region
 }

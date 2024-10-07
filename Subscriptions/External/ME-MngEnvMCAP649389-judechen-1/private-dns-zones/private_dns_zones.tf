e522060da00b0875957388ad903ec2c306577053
Jude Chen <judechen@microsoft.com>
2024-10-07 16:40:53 -0500

    fixed rg conflict.

--- Subscriptions/External/ME-MngEnvMCAP649389-judechen-1/private-dns-zones/private_dns_zones.tf
+++ Subscriptions/External/ME-MngEnvMCAP649389-judechen-1/private-dns-zones/private_dns_zones.tf
@@ -2,6 +2,6 @@ module "avm-ptn-network-private-link-private-dns-zones" {
   source  = "Azure/avm-ptn-network-private-link-private-dns-zones/azurerm"
   version = "0.4.0"
   # insert the 2 required variables here
-  resource_group_name = azurerm_resource_group.private-dns-rg.name
+  resource_group_name = "private-dns-rg"
   location            = local.primary_region
 }
--- Subscriptions/External/ME-MngEnvMCAP649389-judechen-1/private-dns-zones/resource_group.tf
+++
@@ -1,4 +0,0 @@
-resource "azurerm_resource_group" "private-dns-rg" {
-  location = local.primary_region
-  name     = "private-dns-rg"
-}
